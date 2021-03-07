ea76660d17bc50c7846de81c6c7770aed708e21b
Your Name <you@example.com>
2021-03-07 14:25:03 +0530

    after changes commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatExpansionModule} from '@angular/material/expansion'; 
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatListModule} from '@angular/material/list'; 
 
-const MaterialComponents = [MatCardModule,MatIconModule, MatButtonModule,MatExpansionModule]
+const MaterialComponents = [MatCardModule,MatIconModule, MatButtonModule,MatExpansionModule,MatDividerModule,MatListModule]
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents]
